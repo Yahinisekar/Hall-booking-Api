f2c27f40cbdbd2a458a2077892fe903faa6e64fa
Yahini <136244447+Yahinisekar@users.noreply.github.com>
2024-03-13 19:58:10 +0530

    new

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const PORT = 4000;
 app.use(cors());
 app.use(express.json())  //for parsing application/json
 
-
+app.get('/', (req, res) => {
+    res.status(200)
+      .send(`<div style="text-align:center;">NodeJs Hall Booking API</div>
+    <ul>
+    <li> Create a room  : <mark>/createRoom</mark> </li>
+    <li> book rooms   : <mark>/bookroom </mark></li>
+    <li> List booked rooms   : <mark>/bookedrooms </mark></li>
+    <li> List customers   : <mark>/customer </mark></li>
+    <li> List booking count   : <mark>/bookingcount </mark></li>
+    </ul>`);
+})
 
 app.use('/api',router);
 app.listen(PORT, () => {
